8af5b43e4c184db2d9aba2650b663be7eabd2d6e
20062443 <20062443@tafe.wa.edu.au>
2024-11-07 14:42:59 +0800

    feat(carpark.py): constructor

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,28 @@
 class CarPark:
-    pass
+   def __init__(self, location, capacity, plates=None, sensors=None, displays=None):
+      self.location = "Unknown"
+      if location is not None and len(location) > 0:
+         self.location = location
+
+      self.capacity = 0
+      if capacity is not None and capacity > 0 and isinstance(capacity, (int,float)):
+         self.capacity = capacity
+
+      self.plates = []
+      if plates is not None:
+         self.plates.append(plates)
+
+      self.sensors = []
+      if sensors is not None:
+         self.sensors.append(sensors)
+
+      self.displays = []
+      if displays is not None:
+         self.displays.append(displays)
+
+   def __str__(self):
+      return f"Car park location ({str(self.location)}), capacity ({str(self.capacity)}). Has {str(len(self.displays))} display and {str(len(self.sensors))} sensors"
+
+if __name__ == "__main__":
+   carpark = CarPark("City", 100)
+   print(carpark)
---
+++ tests/test_car_park.py
@@ -0,0 +1,2 @@
+import car_park
+
