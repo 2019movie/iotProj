26d9f8de07ce49dbe8a615c0eb663d6f368b9671
C T <20062443@tafe.wa.edu.au>
2024-11-28 12:08:48 +0800

    finalise the project

--- tests/test_car_park.py
+++ tests/test_car_park.py
@@ -40,13 +40,8 @@ class TestCarPark(unittest.TestCase):
         self.assertEqual(self.car_park.available_bays, 0)
 
     def test_unlisted_car_leave_car_park(self):
-        for car in range(10):
-            self.car_park.add_car(f"FAKE-{car}")
-        for car in range(12):
-            if car <= 9:
-                self.car_park.remove_car(f"FAKE-{car}")
-            if car > 9:
-                self.assertRaises(ValueError, self.car_park.remove_car, f"FAKE-{car}")
+        for car in range(10, 20):
+            self.assertRaises(ValueError, self.car_park.remove_car, f"FAKE-{car}")
 
 
     def test_removing_a_car_that_does_not_exist(self):
@@ -54,17 +49,17 @@ class TestCarPark(unittest.TestCase):
             self.car_park.remove_car("NO-1")
 
     def test_log_file_created(self):
-        new_carpark = CarPark("123 Example Street"
-                              , 100
-                              , log_file=self.test_log_file)
+        new_carpark = CarPark("123 Example Street",
+                               100,
+                               log_file=self.test_log_file)
         self.assertTrue(Path(self.test_log_file).exists())
 
 
 
     def test_car_logged_when_entering(self):
-        new_carpark = CarPark("123 Example Street"
-                              , 100
-                              , log_file=self.test_log_file)
+        new_carpark = CarPark("123 Example Street",
+                               100,
+                               log_file=self.test_log_file)
         self.car_park.add_car("NEW-001")
         with self.car_park.log_file.open() as f:
             last_line = f.readlines()[-1]
@@ -73,9 +68,9 @@ class TestCarPark(unittest.TestCase):
         self.assertIn("\n", last_line)  # check entry has a new line
 
     def test_car_logged_when_exiting(self):
-        new_carpark = CarPark("123 Example Street"
-                              , 100
-                              , log_file = self.test_log_file)
+        new_carpark = CarPark("123 Example Street",
+                               100,
+                               log_file = self.test_log_file)
         self.car_park.add_car("NEW-001")
         self.car_park.remove_car("NEW-001")
         with self.car_park.log_file.open() as f:
@@ -85,18 +80,18 @@ class TestCarPark(unittest.TestCase):
         self.assertIn("\n", last_line)  # check entry has a new line
 
     def test_write_config(self):
-        new_carpark = CarPark("Mooning place"
-                              , 34
-                              , log_file = self.test_log_file)
+        new_carpark = CarPark("Mooning place",
+                               34,
+                               log_file = self.test_log_file)
         new_carpark.write_config()
         with new_carpark.config_file.open() as file:
             line = file.readlines()
         self.assertEqual('{"location": "Mooning place", "capacity": 34, "log_file": "test_log.txt"}',line[0])
 
     def test_from_config(self):
-        new_carpark = CarPark("Mooning place test"
-                              , 45
-                              , log_file = self.test_log_file)
+        new_carpark = CarPark("Mooning place test",
+                               45,
+                               log_file = self.test_log_file)
         new_carpark.write_config()
         carpark_from_config = CarPark.from_config(new_carpark.config_file)
         self.assertEqual(carpark_from_config.location, "Mooning place test")
